b3af28933c9692f088192fa74257165deeeb5e0b
Tuan Tran <tuan.tran1122@gmail.com>
2016-10-14 12:27:57 -0500

    Troubleshooting

--- game.js
+++ game.js
@@ -1,58 +1,33 @@
 module.exports = function(){	
+	var chosenBank;
+	var wordBankFF = ["Black Mage", "White Mage"],
+		wordBankFF7 = ["Cloud", "Tifa", "Barret", "Aeris", "Cid", "Vincent"],
+		wordBankFF12 = ["Vaan", "Balthier", "Fran", "Basch", "Ashe", "Penelo", "Reks"],
+		wordBankFF13 = ["Lightning", "Snow", "Vanille", "Sazh", "Hope", "Fang"];
+
+	var randomWord = function(wordbank) {
+		return wordbank[Math.floor(Math.random() * wordbank.length)]
+	};
+	
 	this.pickYourCategory = function(category){
-		var chosenBank;
-		var wordBankFF7 = ["Cloud", "Tifa", "Barret", "Aeris", "Cid", "Vincent"];
-		var wordBankFF12 = ["Vaan", "Balthier", "Fran", "Basch", "Ashe", "Penelo", "Reks"];
-		var wordBankFF13 = ["Lightning", "Snow", "Vanille", "Sazh", "Hope", "Fang"];
-			
-		var randomWord = function(wordbank) {
-			return wordBank[Math.floor(Math.random() * wordBank.length)]
-		};
-		
 
 		switch (category) {
+			case "Final Fantasy":
+				chosenBank = randomWord(wordBankFF);
+				break;
 			case "Final Fantasy 7":
-				chosenBank = this.randomWord(wordBankFF7);
-				console.log("Did it break?");
+				chosenBank = randomWord(wordBankFF7);
 				break;
 			case "Final Fantasy 12":
-				chosenBank = this.randomWord(wordBankFF12);
+				chosenBank = randomWord(wordBankFF12);
 				break;
 			case "Final Fantasy 13":
-				chosenBank = this.randomWord(wordBankFF13);
+				chosenBank = randomWord(wordBankFF13);
 				break;
 			default:
 				console.log("Error");
 				break;
 		}
-		return this.chosenBank;
+		return chosenBank;
 	}
 }
-
-// var wordBankFF7 = ["Cloud", "Tifa", "Barret", "Aeris", "Cid", "Vincent"];
-// var wordBankFF12 = ["Vaan", "Balthier", "Fran", "Basch", "Ashe", "Penelo", "Reks"];
-// var wordBankFF13 = ["Lightning", "Snow", "Vanille", "Sazh", "Hope", "Fang"];
-
-// var randomWord = function(wordbank) {
-// 	return wordBank[Math.floor(Math.random() * wordBank.length)]
-// };
-
-// module.exports.pickYourCategory = function(category){
-// 	var chosenBank;
-// 	switch (category) {
-// 		case "Final Fantasy 7":
-// 			chosenBank = randomWord(wordBankFF7);
-// 			console.log("Did it break?");
-// 			break;
-// 		case "Final Fantasy 12":
-// 			chosenBank = randomWord(wordBankFF12);
-// 			break;
-// 		case "Final Fantasy 13":
-// 			chosenBank = randomWord(wordBankFF13);
-// 			break;
-// 		default:
-// 			console.log("Error");
-// 			break;
-// 	}
-// 	return chosenBank;
-// }
--- main.js
+++ main.js
@@ -1,42 +1,106 @@
 var inquirer = require("inquirer"),
 	Game = require("./game.js"),
-	Word = require("./word.js"),
+ 	Word = require("./word.js"),
 	Letter = require("./letter.js");
 
+var wins = 0,
+	losses = 0,
+	guessesRemaining = 10;
 
-console.log("Just how well do you know your Final Fantasy Characters names?");
-inquirer.prompt([
-	{
-		type: "input",
-		name: "name",
-		message: "What is your name?"
+function reset(){
+	guessesRemaining = 10;
+	categoryQuestion();
+}
+
+function userCategory(choosenCategory){
+	console.log("\nYou have chosen the category " + choosenCategory.ffChoices);
+	var gameObj = new Game();
+	var currentWord = gameObj.pickYourCategory(choosenCategory.ffChoices);
+	console.log("Current word is " + currentWord); //Remove Later
+
+	userLetterGuess(currentWord);
+}
+
+function userLetterGuess(currentWord) {
+	if (guessesRemaining > 0){
+		inquirer.prompt([
+			{
+				type: "input",
+				name: "guessLetter",
+				message: "Guess a letter."
+			}
+		]).then(function(guess){
+			console.log("\nYou guess the letter: " + guess.guessLetter);
+			
+			// Need to figure out how to call word/letter.js 
+			// if(){
+				// right section
+			// }else{
+				// wrong section (Decrement guessesRemaining.)
+			// }
+			console.log("Random stuff " + currentWord);
+			var randomthing = new Word(currentWord);
+			console.log(randomthing);
+			// console.log(Word.show(currentWord));
+
+			// var wordToGuess = this.currentWord.show();
+			// console.log(Word.show(currentWord));
+			userLetterGuess(currentWord);
+		})		
+	}else{
+		tryAgainOrLeave();
 	}
-]).then(function(user){
+}
+
+function tryAgainOrLeave(){
+	inquirer.prompt([
+		{
+			type: "confirm",
+			name: "tryAgain",
+			message: "Would you like to try again?"
+		}
+	]).then(function(userChoice){
+		if (userChoice.tryAgain == true){
+			reset();
+		}else{
+			console.log("Bye!");
+		}
+	})
+}
+
+function start(){
+	console.log("***************************************************************" +
+		"\n***************************************************************" +
+		"\n************   Welcome to Final Fantasy Hangman!   ************" +
+		"\n***************************************************************" +
+		"\n***************************************************************")
+
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "Please state is your name to begin."			
+		}
+	]).then(function(user){
+		console.log("\nThanks " + user.name + "!\n");
+		categoryQuestion();
+	})
+}
+
+function categoryQuestion(){
 	inquirer.prompt([
 		{
 			type: "list",
 			name: "ffChoices",
-			message: "Welcome " + user.name + "! What Final Fantasy series would you like to guess from?",
-			choices: ["Final Fantasy 7", "Final Fantasy 12", "Final Fantasy 13", "Bamn"]
+			message: "What Final Fantasy series would you like to guess from?\n",
+			choices: ["Final Fantasy", "Final Fantasy 7", "Final Fantasy 12", "Final Fantasy 13", "Bamn"]
 		}
-	])
-	.then(function(choosenCategory){
-		console.log(choosenCategory.ffChoices);
-		var currentWord = new Game.pickYourCategory(choosenCategory.ffChoices);
-		console.log(currentWord);
-		// console.log("Did it break?");
+	]).then(function(choosenCategory){
+		userCategory(choosenCategory);
 	})
-})
+};
 
-
-
-	// .then(function(choosenCategory){
- //        console.log(choosenCategory.ffChoices);
- //        var currentWord = new Game.pickYourCategory(choosenCategory.ffChoices);
- //        console.log(currentWord);
- //        // console.log("Did it break?");
- //    }).catch(function(err){ //-------this
- //        console.log(err);
- //    })
-
-
+start();
+			// .catch(function(err){
+			// 	console.log(err);
+			// })
